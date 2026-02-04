1daa6cb187bacdcfbda909a336e66529124066a1
goraydev <jofre1236@gmail.com>
2026-02-04 11:00:10 -0500

    Iniciando nuevo proyecto funcional dark/light

---
+++ 07-componentesapp/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Slot } from "expo-router";
+import "../global.css";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Slot />
+    </ThemeProvider>
+  );
+}
---
+++ 07-componentesapp/app/home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="bg-gray-300 dark:bg-gray-800 flex-1">
+      <Text className="text-red-500 text-2xl dark:text-light">Holaa</Text>
+    </View>
+  );
+}
