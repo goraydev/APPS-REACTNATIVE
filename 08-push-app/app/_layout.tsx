0a9c264ea4bacdf6d4009a4cbf3a6e01d356562e
goraydev <jofre1236@gmail.com>
2026-02-09 10:38:02 -0500

    instalando eas al proyecto pushapp

--- 08-push-app/app/_layout.tsx
+++ 08-push-app/app/_layout.tsx
@@ -2,10 +2,8 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import '../global.css';
 import { Stack } from 'expo-router';
 import { useColorScheme } from 'nativewind';
-<<<<<<< HEAD
-=======
 import { useEffect } from 'react';
->>>>>>> parent of 9eb1a82 (Incluyendo eas a push notifications)
+
 
 export default function Layout() {
   const { colorScheme, setColorScheme } = useColorScheme();
