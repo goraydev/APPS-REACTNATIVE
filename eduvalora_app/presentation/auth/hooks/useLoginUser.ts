e7184f71ee60c0841534b553de69542ac091e706
goraydev <jofre1236@gmail.com>
2026-03-23 12:40:05 -0500

    Dando funcionalidad de ingresar al sistema

--- eduvalora_app/presentation/auth/hooks/useLoginUser.ts
+++ eduvalora_app/presentation/auth/hooks/useLoginUser.ts
@@ -2,13 +2,14 @@ import { login } from '@/core/auth/actions/auth-action';
 import { UserLogin } from '@/core/auth/interfaces/user';
 import { useMutation } from '@tanstack/react-query';
 import { Alert } from 'react-native';
+import { useAuthStore } from '../store/store';
 
 export const useLoginUser = () => {
+  const changeStatus = useAuthStore((state) => state.changeStatus);
   const mutation = useMutation({
     mutationFn: (form: UserLogin) => login(form),
     onSuccess: (data) => {
-      console.log('exito al iniciar sesión', data);
-      
+      changeStatus(data);
     },
     onError: (error: Error) => {
       Alert.alert('Error', error.message);
--- eduvalora_app/presentation/auth/store/store.ts
+++ eduvalora_app/presentation/auth/store/store.ts
@@ -1,12 +1,20 @@
-import { User } from '@/core/auth/interfaces/user';
+import { login } from '@/core/auth/actions/auth-action';
+import { User, UserLogin, UserResponse, Usuario } from '@/core/auth/interfaces/user';
+import { SecureStorageAdapter } from '@/helpers/adapters/secure-storage';
 import { create } from 'zustand';
+
+export type AuthStatus = 'authenticated' | 'unauthenticated' | 'checking';
 
 export interface AuthState {
   newUser: User;
   setNewUser: (newUser: User) => void;
+  user: UserResponse | null;
+  status: AuthStatus;
+  changeStatus: (user: UserResponse) => Promise<boolean>;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
+export const useAuthStore = create<AuthState>((set, get) => ({
+  status: 'unauthenticated',
   newUser: {
     dni: 0,
     id_faculty: '',
@@ -14,5 +22,16 @@ export const useAuthStore = create<AuthState>((set) => ({
     email: '',
     password: '',
   },
+  user: null,
   setNewUser: (payload) => set((state) => ({ newUser: payload })),
+  changeStatus: async (user: UserResponse) => {
+    if (!user) {
+      set({ status: 'unauthenticated', user: null });
+      await SecureStorageAdapter.deleteItem('token');
+      return false;
+    }
+    set({ status: 'authenticated', user: user });
+    await SecureStorageAdapter.setItem('token', user.token);
+    return true;
+  },
 }));
