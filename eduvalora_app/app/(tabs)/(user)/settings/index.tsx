6a8b00e3ee5da8e38f319428f46886f6b9f67d60
goraydev <jofre1236@gmail.com>
2026-03-24 20:21:42 -0500

    Diseñando app de ajustes

--- eduvalora_app/app/(tabs)/(user)/settings/index.tsx
+++ eduvalora_app/app/(tabs)/(user)/settings/index.tsx
@@ -5,6 +5,7 @@ import ThemedSwitch from '@/presentation/theme/components/ThemedSwitch';
 import ThemedText from '@/presentation/shared/ThemedText';
 import { useAuthStore } from '@/presentation/auth/store/store';
 import ThemedButton from '@/presentation/shared/ThemedButton';
+import { View } from 'react-native';
 
 export default function SettingsScreen() {
   const { logout, user } = useAuthStore();
@@ -20,7 +21,11 @@ export default function SettingsScreen() {
         <ThemedText type="h2" className="my-4">
           Ajustes de Perfil
         </ThemedText>
-        <ThemedText>Usuario: {user?.usuario.username}</ThemedText>
+        <View className="mb-4 flex flex-row border-2 border-gray-950 p-4">
+          <ThemedText type="semibold">Usuario: </ThemedText>
+          <ThemedText type="semibold">{user?.usuario.username}</ThemedText>
+        </View>
+
         <ThemedButton text="Cerrar Sesión" onPress={logout} icon="log-out-outline" />
       </ThemedView>
     </>
