f762807bb208275a0cb31b138a0d0ac78153bbb7
goraydev <jofre1236@gmail.com>
2026-03-25 20:14:43 -0500

    Mejorando interfaz de home

--- eduvalora_app/app/(tabs)/(stack)/home/index.tsx
+++ eduvalora_app/app/(tabs)/(stack)/home/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import ThemedText from '@/presentation/shared/ThemedText';
+import ThemedView from '@/presentation/shared/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <ThemedView padding>
+      <ThemedText>HomeScreen</ThemedText>
+    </ThemedView>
   );
 }
