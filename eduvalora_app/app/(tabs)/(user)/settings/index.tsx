62bbf363877172bc15a25af6ce047d0b50cddea1
goraydev <jofre1236@gmail.com>
2026-03-26 22:33:52 -0500

    Creando formulario de edicion de perfil

--- eduvalora_app/app/(tabs)/(user)/settings/index.tsx
+++ eduvalora_app/app/(tabs)/(user)/settings/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ThemedHeader from '@/presentation/shared/ThemedHeader';
 import ThemedView from '@/presentation/shared/ThemedView';
 import ThemedSwitch from '@/presentation/theme/components/ThemedSwitch';
@@ -6,9 +6,18 @@ import ThemedText from '@/presentation/shared/ThemedText';
 import { useAuthStore } from '@/presentation/auth/store/store';
 import ThemedButton from '@/presentation/shared/ThemedButton';
 import { View } from 'react-native';
+import ThemedTextInput from '@/presentation/shared/ThemedTextInput';
+import ThemedActivity from '@/presentation/shared/ThemedActivity';
 
 export default function SettingsScreen() {
   const { logout, user } = useAuthStore();
+  const [boxUserName, setBoxUsername] = useState('');
+  const [boxEmail, setboxEmail] = useState();
+
+  if (!user) {
+    return <ThemedActivity />;
+  }
+  const { username, email } = user.usuario;
 
   return (
     <>
@@ -21,16 +30,24 @@ export default function SettingsScreen() {
         <ThemedText type="h2" className="my-4">
           Ajustes de Perfil
         </ThemedText>
-        <View className="mb-4 flex flex-row border-2 border-gray-950 p-4">
-          <ThemedText type="semibold">Usuario: </ThemedText>
-          <ThemedText type="semibold">{user?.usuario.username}</ThemedText>
+        <View className="flex flex-col gap-4">
+          <ThemedText type="semibold">Nombre de Usuario: </ThemedText>
+          <ThemedTextInput
+            value={username}
+            onChangeText={(text) => setBoxUsername(text)}
+            placeholder="Username"
+          />
         </View>
-        <View className="mb-4 flex flex-row border-2 border-gray-950 p-4">
-          <ThemedText type="semibold">Correo: </ThemedText>
-          <ThemedText type="semibold">{user?.usuario.email}</ThemedText>
+        <View className="my-4 flex flex-col gap-4">
+          <ThemedText type="semibold">Email: </ThemedText>
+          <ThemedTextInput
+            value={email}
+            onChangeText={(text) => setBoxUsername(text)}
+            placeholder="Username"
+          />
         </View>
 
-        <ThemedButton text="Cerrar Sesión" onPress={logout} icon="log-out-outline" />
+        <ThemedButton text="Actualizar" onPress={logout} />
       </ThemedView>
     </>
   );
